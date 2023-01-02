35caf61d4b0c0f03c3d89e95c2785f604250ec7e
nitish0104 <nitishdalvi1@gmail.com>
2023-01-02 20:30:32 +0530

    client change

--- script.js
+++ script.js
@@ -26,3 +26,44 @@ const handleSubmit = (e) => {
 			})
 	}
 }
+
+// cards slider demo
+
+var swiper = new Swiper(".slide-container", {
+	slidesPerView: 4,
+	spaceBetween: 20,
+	sliderPerGroup: 4,
+	loop: true,
+	autoplay: { delay: 1000 },
+	centerSlide: "true",
+	fade: "true",
+	grabCursor: "true",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		520: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1000: {
+			slidesPerView: 4,
+		},
+	},
+});
+
+
+
+
